f16d325fd47280c97e6bfba7504cd66dbdbedfea
Lalo <villaseduardo@hotmail.com>
2016-10-18 19:28:05 -0300

    error cuando dos valores son negativos

--- ej6/vector_poblacional.m
+++ ej6/vector_poblacional.m
@@ -1,11 +1,12 @@
-function [ vec_pob ] = vector_poblacional( n1,n2,n3,n4,c,ras )
+function [ vec_pob,angulo ] = vector_poblacional( n1,n2,n3,n4,c,ras )
 %UNTITLED Summary of this function goes here
 %      Detailed explanation goes here
-    vec_pob = zeros(4,24);
+    vec_pob = zeros(4,1);
     vec_pob(1,:) =  ras(1)/ get_rmax(n1) * c(1);
     vec_pob(2,:) =    ras(2) /get_rmax(n2) * c(2);
     vec_pob(3,:) =  ras(3) /get_rmax(n3) * c(3);
     vec_pob(4,:) =  ras(4)  /get_rmax(n4)* c(4);
-    
+    variable = sum(vec_pob);
+    angulo = radtodeg(acos(variable));
 end
  
