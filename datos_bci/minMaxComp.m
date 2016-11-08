d0269734918ba7f749cfab9c5ab72b4fd1617f2e
Lalo <villaseduardo@hotmail.com>
2016-11-08 13:37:49 -0300

    devuelve indices, valores,status. Recibo treshold,se ve final :3

--- datos_bci/minMaxComp.m
+++ datos_bci/minMaxComp.m
@@ -1,19 +1,23 @@
-function [  valoresDisparos ] = minMaxComp( ambos )
-     threshold = getThreshold(ambos);
+function [  valoresDisparos,indices,rta ] = minMaxComp( ambos , upTreshold, downTreshold)
+    rta = 1; 
+    %threshold = getThreshold(ambos);
      pendientes = diff(ambos); %Pendientes
      %Ahora encontramos los cambios de pendientes
      i = 1 ; 
      nMax = 0;
      valoresDisparos = 0;
      while( i < (length(ambos)-10) )
-        maximo=0;
-        minimo=0;
+        maximo=1;
+        minimo=1;
+        iMin = 1 ;
+        iMax = 1;
          %Primero encuentro un maximo
         while(pendientes(i)<pendientes(i+1) )
             i = i+1;
         end
-        if(ambos(i) > threshold)     
+        if(ambos(i) > upTreshold)     
             maximo  = ambos(i);
+            iMax = i;
             i = i + 1;
         end
         %guardar solo si ambos cumplen
@@ -22,19 +26,22 @@ function [  valoresDisparos ] = minMaxComp( ambos )
         while(pendientes(i) < pendientes(i+1) && i < length(ambos) )
             i = i+1;
         end
-        if(abs(ambos(i)) > abs(threshold))
+        if( ambos(i) < downTreshold)
             minimo = ambos(i);
+            iMin = i;
             i = i+1;
         end
-        if(minimo < -threshold &&  maximo > threshold)
+        if( (minimo < downTreshold) && ( maximo > upTreshold))
             nMax = nMax+1;
-            valoresDisparos(nMax,1) = minimo;
-            valoresDisparos(nMax,2) = maximo;  
+            indices(nMax,2) = iMin;
+            indices(nMax,1) = iMax;
+            valoresDisparos(nMax,2) = minimo;
+            valoresDisparos(nMax,1) = maximo;  
         end
         i = i+1;
      end
      if(valoresDisparos == 0 ) 
-        valoresDisparos = 'Asimetria';
+        rta = 0;
      end
  end
     
